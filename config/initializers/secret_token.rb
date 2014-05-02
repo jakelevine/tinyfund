26754cd0b57d57784090525342f5ca34f2d4091a
jakelevine <levine.jake@gmail.com>
2014-05-02 11:59:23 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tinyfund::Application.config.secret_key_base = '026c727821777f025cdb1c5ac8d452cebeaf4b01b5dffa234639e56a4050ae53aacec01701e96c111eca8f595b8b226578e980d037ac5a67aaca998ca3a06af5'
